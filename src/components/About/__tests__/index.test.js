6ae9a742b73bd9182eafb2694d5635c6714d9378
Caitlin O'Reilly <coreilly29@gmail.com>
2022-12-09 21:36:44 -0500

    added first test to index.test.js file

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react";
 
 // render function renders component - jest creates simulated DOM in node.js environment to approximate the DOM
-// cleanup function used to remove components from DOM to prevent memroy leaking 
-import { render, cleanup} from '@testing-library/react';
+// cleanup function used to remove components from DOM to prevent memroy leaking
+// cleanup also used to prevent variable or data collisions betw tests that could corrupt test results
+import { render, cleanup } from "@testing-library/react";
 
-// import extend-expect lib from jest-dom package 
-import '@testing-library/jest-dom/extend-expect';
+// import extend-expect lib from jest-dom package
+import "@testing-library/jest-dom/extend-expect";
 
-// import the component we'll be testing - the About component 
-import About from '..';
+// import the component we'll be testing - the About component
+import About from "..";
 
 // call the cleanup function
 afterEach(cleanup);
 
-// use describe function to declare component we're testing 
-describe('About component', () => {
-    //renders About test
-
-    // First test
-
-    // Second test 
-})
+// use describe function to declare component we're testing
+describe("About component", () => {
+  //renders About test
+  // First test
+  it("renders", () => {
+    render(<About />);
+  });
+});
